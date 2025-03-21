781499bceb4c804877468c5e04f7a53111c5cf7f
Sot. Tzelatidis <stzelati@gmail.com>
2025-03-21 20:13:09 +0200

    feat: new LoadSave class added with GetSpriteAtlas method to get an image.  LoadSave class and GetSpriteAtlas method replaced raw code in loadAnimation to get the img

---
+++ src/utils/LoadSave.java
@@ -0,0 +1,33 @@
+package utils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Load and Save the game
+ */
+public class LoadSave {
+
+    public static final String PLAYER_ATLAS = "player_sprites.png";
+
+    public static BufferedImage GetSpriteAtlas(String fileName) {
+        BufferedImage img = null;
+        InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+        try {
+            img = ImageIO.read(is);
+
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally{
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return img;
+    }
+}
