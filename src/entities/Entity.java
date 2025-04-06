77abd51dc2a5b094f6e622068a951ea05e21dfc8
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-06 12:57:54 +0300

    feat: hitbox around player created

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.awt.*;
+
 /**
  * All entities of the game. Player, enemies etc.
  */
@@ -8,6 +10,7 @@ public abstract class Entity {
     protected float y;
     protected int width;
     protected int height;
+    protected Rectangle hitbox;
 
     public Entity(float x, float y, int width, int height) {
         this.x = x;
@@ -15,5 +18,27 @@ public abstract class Entity {
         this.width = width;
         this.height = height;
 
+        initHitbox();
+
+    }
+
+    protected void drawHitbox(Graphics g) {
+        // For debugging the hitbox
+        g.setColor(Color.RED);
+        g.drawRect(hitbox.x, hitbox.y, hitbox.width, hitbox.height);
+    }
+
+    private void initHitbox() {
+        hitbox = new Rectangle((int)x, (int)y, width, height);
+
+    }
+
+    protected void updateHitbox() {
+        hitbox.x = (int)x;
+        hitbox.y = (int)y;
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
     }
 }
